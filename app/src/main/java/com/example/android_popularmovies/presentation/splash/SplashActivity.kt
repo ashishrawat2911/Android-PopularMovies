67230ab9a726933610e1cb9ceb56d46f37b5fbea
ashrawat2 <ashish.rawat@publicissapient.com>
2022-06-13 15:08:15 +0530

    Added coroutineScope in Splash

--- app/src/main/java/com/example/android_popularmovies/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/example/android_popularmovies/presentation/splash/SplashActivity.kt
@@ -3,11 +3,12 @@ package com.example.android_popularmovies.presentation.splash
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.coroutineScope
 import com.example.android_popularmovies.R
 import com.example.android_popularmovies.presentation.movie.view.MainActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
@@ -15,12 +16,15 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Handler(Looper.myLooper()!!).postDelayed({
+        lifecycle.coroutineScope.launch {
+            delay(DELAY_IN_IN_TRANSITION)
+            navigate()
+        }
+    }
 
-
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, DELAY_IN_IN_TRANSITION)
+    private fun navigate() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 
     companion object {
